a611d93804cc30b5cdea1aea3a0ab874f876decf
Craig Olson <craig.olson804@gmail.com>
2023-03-03 11:28:10 -0800

    community not found page

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -4,12 +4,16 @@ import { doc, getDoc } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
 import safeJsonStringify from "safe-json-stringify";
 import React from "react";
+import CommunityNotFound from "@/src/components/Community/NotFound";
 
 type CommunityPageProps = {
   communityData: Community;
 };
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  if (!communityData) {
+    return <CommunityNotFound />;
+  }
   return <div>WELCOME TO {communityData.id}</div>;
 };
 
@@ -24,9 +28,11 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
     return {
       props: {
-        communityData: JSON.parse(
-          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
-        ),
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
       },
     };
   } catch (error) {
